9e7ea7572543368d3d3a7be33a7cf7b04e90a3bd
rdiaz522 <ronarniediaz@gmail.com>
2025-01-19 12:16:00 +0800

    Version 1.0 added new component

---
+++ app/src/components/Links.vue
@@ -0,0 +1,36 @@
+<script setup>
+defineProps({
+  imgSrc: {
+    type: String,
+    default: '',
+  },
+  imgType: {
+    type: String,
+    default: 'img',
+  },
+  label: {
+    type: String,
+    default: '',
+  },
+  url: {
+    type: String,
+    default: '',
+  },
+})
+</script>
+<style scoped></style>
+<template>
+  <div class="flex flex-row p-5">
+    <div v-if="imgType === 'img'">
+      <img class="h-10 w-10" :src="imgSrc" />
+    </div>
+    <div v-if="imgType === 'svg'">
+      <slot></slot>
+    </div>
+    <div class="pl-3 pt-2">
+      <a :href="url" target="_blank">
+        <h1 class="text-lg text-teal-700 dark:text-teal-300">{{ label }}</h1></a
+      >
+    </div>
+  </div>
+</template>
--- app/src/views/Contact.vue
+++ app/src/views/Contact.vue
@@ -1,9 +1,70 @@
-<script setup></script>
+<script setup>
+import Links from '@/components/Links.vue'
+</script>
 <style scoped></style>
 <template>
-  <div class="lg:h-screen lg:w-screen">
-    <h1 class="flex mt-36 mb-12 text-2xl font-bold margin-left-right">
-      REACH OUT TO ME AT:
-    </h1>
+  <div class="lg:h-screen lg:w-screen margin-left-right">
+    <h1 class="flex mt-36 mb-12 text-2xl font-bold">REACH OUT TO ME AT:</h1>
+    <div class="flex flex-row">
+      <div>
+        <Links
+          label="ronarniediaz@gmail.com"
+          img-src="https://img.icons8.com/?size=100&id=P7UIlhbpWzZm&format=png&color=000000"
+          url="https://mail.google.com/mail/u/0/?fs=1&to=ronarniediaz@gmail.com&su=SUBJECT&body=BODY&tf=cm"
+        />
+      </div>
+      <div>
+        <Links label="GitHub" img-type="svg" url="https://github.com/rdiaz522">
+          <svg
+            class="dark:fill-white h-12 w-12"
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width="100"
+            height="100"
+            viewBox="0 0 48 48"
+          >
+            <path
+              d="M 24 4 C 12.972066 4 4 12.972074 4 24 C 4 35.027926 12.972066 44 24 44 C 35.027934 44 44 35.027926 44 24 C 44 12.972074 35.027934 4 24 4 z M 24 7 C 33.406615 7 41 14.593391 41 24 C 41 31.66536 35.956939 38.122519 29 40.251953 L 29 35.136719 C 29 33.226635 27.899316 31.588619 26.308594 30.773438 A 10 8 0 0 0 32.4375 18.720703 C 32.881044 17.355414 33.376523 14.960672 32.199219 13.076172 C 29.929345 13.076172 28.464667 14.632086 27.765625 15.599609 A 10 8 0 0 0 24 15 A 10 8 0 0 0 20.230469 15.59375 C 19.529731 14.625773 18.066226 13.076172 15.800781 13.076172 C 14.449711 15.238817 15.28492 17.564557 15.732422 18.513672 A 10 8 0 0 0 21.681641 30.779297 C 20.3755 31.452483 19.397283 32.674042 19.097656 34.15625 L 17.783203 34.15625 C 16.486203 34.15625 15.98225 33.629234 15.28125 32.740234 C 14.58925 31.851234 13.845172 31.253859 12.951172 31.005859 C 12.469172 30.954859 12.144453 31.321484 12.564453 31.646484 C 13.983453 32.612484 14.081391 34.193516 14.650391 35.228516 C 15.168391 36.160516 16.229687 37 17.429688 37 L 19 37 L 19 40.251953 C 12.043061 38.122519 7 31.66536 7 24 C 7 14.593391 14.593385 7 24 7 z"
+            ></path>
+          </svg>
+        </Links>
+      </div>
+      <div>
+        <Links label="Dev.to" img-type="svg" url="https://dev.to/rondev522">
+          <svg
+            class="h-12 w-12 dark:fill-white"
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width="100"
+            height="100"
+            viewBox="0 0 32 32"
+          >
+            <path
+              d="M 2 7 L 2 25 L 30 25 L 30 7 L 2 7 z M 4 9 L 28 9 L 28 23 L 4 23 L 4 9 z M 6 11 L 6 21 L 9 21 C 10.654 21 12 19.654 12 18 L 12 14 C 12 12.346 10.654 11 9 11 L 6 11 z M 16 11 C 14.897 11 14 11.897 14 13 L 14 19 C 14 20.103 14.897 21 16 21 L 18 21 L 18 19 L 16 19 L 16 17 L 18 17 L 18 15 L 16 15 L 16 13 L 18 13 L 18 11 L 16 11 z M 19.691406 11 L 21.775391 20.025391 C 21.907391 20.595391 22.415 21 23 21 C 23.585 21 24.092609 20.595391 24.224609 20.025391 L 26.308594 11 L 24.255859 11 L 23 16.439453 L 21.744141 11 L 19.691406 11 z M 8 13 L 9 13 C 9.552 13 10 13.448 10 14 L 10 18 C 10 18.552 9.552 19 9 19 L 8 19 L 8 13 z"
+            ></path>
+          </svg>
+        </Links>
+      </div>
+
+      <div>
+        <Links label="X | Twitter" img-type="svg" url="https://x.com/rondev522">
+          <svg
+            class="h-12 w-12 dark:fill-white"
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width="100"
+            height="100"
+            viewBox="0 0 50 50"
+          >
+            <path
+              d="M 11 4 C 7.134 4 4 7.134 4 11 L 4 39 C 4 42.866 7.134 46 11 46 L 39 46 C 42.866 46 46 42.866 46 39 L 46 11 C 46 7.134 42.866 4 39 4 L 11 4 z M 13.085938 13 L 21.023438 13 L 26.660156 21.009766 L 33.5 13 L 36 13 L 27.789062 22.613281 L 37.914062 37 L 29.978516 37 L 23.4375 27.707031 L 15.5 37 L 13 37 L 22.308594 26.103516 L 13.085938 13 z M 16.914062 15 L 31.021484 35 L 34.085938 35 L 19.978516 15 L 16.914062 15 z"
+            ></path>
+          </svg>
+        </Links>
+      </div>
+    </div>
   </div>
 </template>
